a1fbd41ee5da06da8861a7f7e12928e65f693fef
Vinay Nakkina <vinay.1@iitj.ac.in>
2022-12-24 08:35:49 +0530

    Added Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { MembersComponent } from './members/members.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'persons', component: MembersComponent },
+];
 @NgModule({
-  declarations: [AppComponent, ProjectsComponent, MembersComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    MembersComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
